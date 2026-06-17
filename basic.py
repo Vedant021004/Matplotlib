56875016a17798f259ad516c5e6ba065eaa2db17
Vedant021004 <vedantkp79@gmail.com>
2026-06-17 21:52:58 +0530

    ...

--- basic.py
+++ basic.py
@@ -5,16 +5,21 @@ import pandas as pd
 year = np.array([2000,2001,2002,2003,2004,2005,2006])
 sales = np.array([10,15,20,25,30,35,40])
 
-plt.plot(year, sales)
+plt.plot(year, sales,color="green")
 plt.xlabel("Year")
 plt.ylabel("Sales")
 plt.title("Sales Trend")
 plt.show()
+
+
+
+
 
 import matplotlib.pyplot as plt
 
 x = [1,2,3,4,5]
 y = [10,20,30,40,50]
 
-plt.plot(x,y,color="red")
+plt.plot(x,y,color="red",marker="o")
+plt.grid()
 plt.show()
