714ad3d266f5e6ba77d97ef8b9138ef8cfc903c6
Vedant021004 <vedantkp79@gmail.com>
2026-06-19 01:31:01 +0530

    ...

--- Panda.graph.py
+++ Panda.graph.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+plt.style.available
+plt.style.use('tableau-colorblind10')
+
 # df = pd.read_csv("sharma-kohli.csv")
 # plt.plot(df['index'], df['V Kohli'],color = "green",marker = 'o', label = 'virat')
 # plt.plot(df['index'], df['RG Sharma'],color = "black",marker = 'o',label = 'Rohit')
@@ -11,14 +14,15 @@ import matplotlib.pyplot as plt
 
 import pandas as pd
 import matplotlib.pyplot as plt
+plt.style.use('tableau-colorblind10')
 
 df = pd.read_csv('batsman_season_record.csv')
 print(df)
 plt.plot(df['batsman'],df['2017'],label='2017')
 plt.scatter(df['batsman'],df['2017'],label='2017')
 plt.bar(df['batsman'],df['2017'],label='2017')
-plt.pie(df['batsman_runs'],labels=df['batsman'],autopct='%0.1f%%')
 plt.grid()
+# plt.savefig("graph.png")
 plt.show()
 
 
@@ -27,5 +31,55 @@ plt.show()
 import pandas as pd
 import matplotlib.pyplot as plt
 df = pd.read_csv('gayle-175.csv')
-plt.pie(df['batsman_runs'],labels=df['batsman'],autopct='%0.1f%%')
+myexplode = [0.1, 0.1, 0.1, 0.1,0.1,0.1]
+plt.pie(df['batsman_runs'],labels=df['batsman'],autopct='%0.1f%%', explode = myexplode, shadow = True)
+plt.savefig("sample.png")
 plt.show()
+
+
+
+
+
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('batsman_season_record.csv')
+
+plt.figure(figsize=(15,5))
+
+plt.subplot(1,3,1)
+plt.plot(df['batsman'], df['2017'])
+plt.title("Line Plot")
+
+plt.subplot(1,3,2)
+plt.bar(df['batsman'], df['2017'])
+plt.title("Bar Plot")
+
+plt.subplot(1,3,3)
+plt.scatter(df['batsman'], df['2017'])
+plt.title("Scatter Plot")
+
+plt.tight_layout()
+plt.show()
+
+
+
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('batter.csv')
+sample_df = df.head(100).sample(25,random_state=5)
+plt.scatter(sample_df['runs'],sample_df['avg'])
+plt.figure(figsize=(18,10))
+plt.scatter(sample_df['avg'],sample_df['strike_rate'],s=sample_df['runs'])
+
+for i in range(sample_df.shape[0]):
+  plt.text(sample_df['avg'].values[i],sample_df['strike_rate'].values[i],sample_df['batter'].values[i])
+plt.colorbar()
+plt.show()
+
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-df = pd.read_csv('gayle-175.csv')
-plt.pie(df['batsman_runs'],labels=df['batsman'],autopct='%0.1f%%')
+
+df = pd.read_csv('batter.csv')
+sample_df = df.head(100).sample(25,random_state=5)
+plt.scatter(sample_df['runs'],sample_df['avg'])
+plt.figure(figsize=(18,10))
+plt.scatter(sample_df['avg'],sample_df['strike_rate'],s=sample_df['runs'])
+
+for i in range(sample_df.shape[0]):
+  plt.text(sample_df['avg'].values[i],sample_df['strike_rate'].values[i],sample_df['batter'].values[i])
+plt.colorbar()
 plt.show()
